9b02370a7ea6b54aed93a0f4e8a5a8f267f663eb
luc <550502661@qq.com>
2018-06-14 23:32:03 +0800

    initialize

---
+++ src/components/lodash/Function.vue
@@ -0,0 +1,73 @@
+<template>
+    <div class="Function-wrap">
+        Function
+    </div>
+</template>
+
+<script>
+
+export default {
+    name: 'Function',
+    data () {
+        return {
+            
+        }
+    },
+    mounted(){
+        //====================================== Function ========================================
+        /**
+         * delay
+         * 延迟函数
+        */
+        _.delay(function(text) {
+        // console.log(text);
+        }, 1000, 'later');
+
+        /**
+         * once
+         * 只调用一次
+        */
+        var initialize = _.once(function(){
+            console.log('调用了一次')
+        });
+        initialize();
+        initialize();
+        initialize();
+        initialize();
+        initialize();
+
+        function doubled(n) {
+            return n * 2;
+        }
+        
+        function square(n) {
+            return n * n;
+        }
+        
+        var func = _.overArgs(function(x, y) {
+            return [x, y];
+        }, [square, doubled]);
+        
+        
+        console.log(func(9, 3))
+        // => [81, 6]
+         console.log(func(10, 5))
+        // => [100, 10]
+
+
+
+
+
+
+
+    },
+    methods:{
+
+	}
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped lang="scss">
+
+</style>
--- src/router/index.js
+++ src/router/index.js
@@ -5,13 +5,15 @@ import array from '@/components/lodash/Array';
 import chain from '@/components/lodash/Chain';
 import collection from '@/components/lodash/Collection';
 import date from '@/components/lodash/Date';
+import Function from '@/components/lodash/Function';
 Vue.use(Router)
 
 const routes = [
 	{ path: '/',name: 'array',component: array },
 	{ path: '/chain',name: 'chain',component: chain },
 	{ path: '/date',name: 'date',component: date },
-	{ path: '/collection',name: 'collection',component: collection }
+	{ path: '/collection',name: 'collection',component: collection },
+	{ path: '/function',name: 'function',component: Function }
 ]
 
 const router = new Router({

90b460cc5c839cc18ee82423ef5f7b33a71ab1cb
Lucy20209060 <550502661@qq.com>
2018-07-05 11:53:55 +0800

    feat:isElement :smirk:

--- src/components/lodash/Lang.vue
+++ src/components/lodash/Lang.vue
@@ -105,11 +105,7 @@ export default {
         * 检查是否是一个DOM元素
         * 
         */
-        console.log(_.isElement(document.body))
-
-
-
-
+         // console.log(_.isElement(document.body))
 
     },
     methods:{

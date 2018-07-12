cca31d1c3429c8c1316e4ea3a14878850ad354d6
Lucy20209060 <550502661@qq.com>
2018-07-12 15:37:05 +0800

    feat:Math中的方法

---
+++ src/components/lodash/Math.vue
@@ -0,0 +1,47 @@
+<template>
+    <div class="Math-wrap">
+        Math
+    </div>
+</template>
+
+<script>
+
+export default {
+    name: 'Math',
+    data () {
+        return {
+            
+        }
+    },
+    mounted(){
+        //====================================== Math ========================================
+        /**
+         * add(augend, addend)
+         * 两个数相加
+        */
+
+        /**
+         * max(array)
+         * 返回数组中最大的值
+         * 如果 array 是空的或者假值则会返回undefined
+         * 
+         * 
+         * min(array)
+         * 返回数组中最小的值
+         * 如果 array 是空的或者假值则会返回undefined
+         * 
+         * sum 求和
+         * 
+         * 
+        */
+    },
+    methods:{
+
+	}
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped lang="scss">
+
+</style>

be32f2dc2949059bf155f16c66d099f041f67fd9
Lucy20209060 <550502661@qq.com>
2018-07-12 15:43:28 +0800

    feat:Number中的方法

---
+++ src/components/lodash/Number.vue
@@ -0,0 +1,33 @@
+<template>
+    <div class="Number-wrap">
+        Number
+    </div>
+</template>
+
+<script>
+
+export default {
+    name: 'Number',
+    data () {
+        return {
+            
+        }
+    },
+    mounted(){
+        //====================================== Number ========================================
+        /**
+         * random
+         * 返回随机数
+        */
+        
+    },
+    methods:{
+
+	}
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped lang="scss">
+
+</style>

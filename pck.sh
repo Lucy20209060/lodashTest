daf96091a0207cafe47f1b6f6f10667938add1bb
Lucy20209060 <550502661@qq.com>
2018-07-28 11:52:09 +0800

    修改pck.sh

--- pck.sh
+++ pck.sh
@@ -1,13 +1,13 @@
 if [ -n "$1" ];then
-echo $1
+echo "\033[31m ==> $1 \033[0m"
 git add -A
-echo "git add -A"
+echo "\033[31m ==> git add -A \033[0m"
 git commit -m "$1"
-echo "git commit $1"
+echo "\033[31m ==> git commit $1 \033[0m"
 git pull
-echo "git pull"
+echo "\033[31m ==> git pull \033[0m"
 git push
-echo "git push"
+echo "\033[31m ==> git push \033[0m"
 echo "\033[31m ==> 代码提交成功 \033[0m"
 else
 echo "\033[31m 请输入commit描述 \033[0m"
